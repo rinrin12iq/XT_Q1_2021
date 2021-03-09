8f90805762821eef6b85f3305bc0495db4004459
Rin <lanpozka@gmail.com>
2021-03-10 00:35:07 +0400

    Task1.2.4

--- Task 1/Task_1_2/Task_1_2_4/Program.cs
+++ Task 1/Task_1_2/Task_1_2_4/Program.cs
@@ -10,6 +10,34 @@ namespace Task_1_2_4
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+
+            Console.Write("Результат: ");
+            Console.Write(Validator(text));
+
+            Console.ReadKey();
+        }
+
+        public static string Validator(string text)
+        {
+            StringBuilder builder = new StringBuilder();
+            int startIndex = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+                {
+                    int firstLetterIndex = startIndex + text.Substring(startIndex, i - startIndex + 1)
+                        .IndexOf(text.Substring(startIndex, i - startIndex + 1).First(j => char.IsLetter(j)));
+
+                    builder.Append(text.Substring(startIndex, i - startIndex + 1));
+                    builder[firstLetterIndex] = char.ToUpper(builder[firstLetterIndex]);
+                    startIndex = i + 1;
+                }
+            }
+
+            return builder.ToString();
         }
     }
 }
